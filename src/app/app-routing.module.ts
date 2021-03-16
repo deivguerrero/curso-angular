726996523e6300a698c770d342de1fb51cb7f2cd
David Guerrero <deivguerrero@gmail.com>
2021-03-16 11:31:45 -0600

    👽 inclusion de una ruta para el componente Product

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductComponent } from './components/product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '/home',
+    component: ProductComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
