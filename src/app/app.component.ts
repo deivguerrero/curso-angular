53fba45632b6b8e132759fc9a132f25c81dcf4ea
David Guerrero <deivguerrero@gmail.com>
2021-03-16 11:53:23 -0600

    👽 separacion de carga de app hacia products (component)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Product } from './product.model'
 
 @Component({
   selector: 'app-root',
@@ -25,57 +24,12 @@ export class AppComponent {
     { "id": "", "label": "Sandia 🍉", "type": "fruta" },
     { "id": "", "label": "Piña 🍍", "type": "fruta" },
     { "id": "", "label": "Brocoli 🥦", "type": "fruta" }];
-  products: Product[] = [
-    {
-      id: '1',
-      image: 'assets/images/camiseta.png',
-      title: 'Camiseta',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '2',
-      image: 'assets/images/hoodie.png',
-      title: 'Hoodie',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '3',
-      image: 'assets/images/mug.png',
-      title: 'Mug',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '4',
-      image: 'assets/images/pin.png',
-      title: 'Pin',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '5',
-      image: 'assets/images/stickers1.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    },
-    {
-      id: '6',
-      image: 'assets/images/stickers2.png',
-      title: 'Stickers',
-      price: 80000,
-      description: 'bla bla bla bla bla'
-    }
-  ];
+
   typeSearch() {
     return this.items.filter(i => { if (i.type === this.itemp.toLocaleLowerCase()) return true });
   };
   regxTag() {
 
   };
-  clickProduct(id: number) {
-    alert(`Vale con ${id}`);
-  };
+
 }
