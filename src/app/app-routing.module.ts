3da96f4c8b053e796091611e02fc44971da270f6
David Guerrero <deivguerrero@gmail.com>
2021-03-16 17:41:22 -0600

    👽 inclusion de redireccion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,11 @@ import { DemoComponent } from './demo/demo.component';
 
 const routes: Routes = [
   {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  {
     path: 'home',
     component: HomeComponent
   },
